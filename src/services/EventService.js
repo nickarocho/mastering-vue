0888d67dc1cc18e5da8bb1d646b30e767b4dd8a4
Nicholas.Arocho <nick@boldlyfine.com>
2020-02-27 10:22:05 -0800

    Install NProgress library, use Axios interceptors to show progress bar

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import NProgress from 'nprogress'
 
 const apiClient = axios.create({
   baseURL: `http://localhost:3000`,
@@ -7,6 +8,26 @@ const apiClient = axios.create({
     Accept: 'application/json',
     'Content-Type': 'application/json'
   }
+})
+
+// APPROACH #1: Axios Interceptors
+// use-cases:
+//   - on request to set authorization tokens
+//   - on response to format or filter data before it reaches into your app
+//   - on response to catch 401 not authorized responses
+// cons:
+//   - not optimal for multiple API calls at the same time
+//   - templates get loaded before API call is returned
+
+// when request is about to go out, start the progress bar
+apiClient.interceptors.request.use(config => {
+  NProgress.start()
+  return config
+})
+// when request returns, finish the progress bar
+apiClient.interceptors.response.use(response => {
+  NProgress.done()
+  return response
 })
 
 export default {
